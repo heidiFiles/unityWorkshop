9c3fdf123e5485428074bad8acfdebd41ff44283
Heidi Cheung <heidicheung07@gmail.com>
2022-06-08 11:05:04 +0200

    commit on main

--- 04Animation/scripts/main.js
+++ 04Animation/scripts/main.js
@@ -5,8 +5,13 @@ var rotationSpeed = 0.01;
 var myOtherBox = document.getElementById('myOtherBox');
 
 function spin(){
-myOtherBox.object3D.rotation.x += rotationSpeed;
+myOtherBox.object3D.rotation.x += rotationSpeed ;
 console.log(myOtherBox.object3D.rotation.x);
 }
+
+function spin(){
+    myOtherBox.object3D.rotation.x += rotationSpeed ;
+    console.log(myOtherBox.object3D.rotation.y);
+    }
 
 setInterval(spin, 16); //equivalent to 60 fps
